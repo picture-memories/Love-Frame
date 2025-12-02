9bd3f4c966d9550949c4ec7f2c040438912d7feb
picture-memories <strxyy73@gmail.com>
2025-12-02 15:45:11 +0200

    Refactor push event handling in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
 self.addEventListener("push", (event) => {
-    let data = {};
-    try {
-        data = event.data.json();
-    } catch(e) {}
+    const data = event.data ? event.data.json() : {};
+
     event.waitUntil(
         self.registration.showNotification(data.title || "Love Frame <3", {
             body: data.body || "Someone sent you love!",
             icon: "/icon.png",
             data: { mediaUrl: data.mediaUrl || null }
-        })
-    );
+        }))
 });
 
 self.addEventListener("notificationclick", (event) => {
     event.notification.close();
-    const mediaUrl = event.notification.data?.mediaUrl;
-    event.waitUntil(clients.openWindow(mediaUrl || "/"));
+
+    const mediaUrl = event.notification.data.mediaUrl;
+
+    if (mediaUrl) {
+        event.waitUntil(clients.openWindow(mediaUrl));
+    } else {
+        event.waitUntil(clients.openWindow("/"));
+    }
 });
